096d73e08893a9a7bb024117dd20c2eb1f67c9b8
kristianp24 <kristianprendi29@gmail.com>
2024-10-09 12:51:52 +0300

    second commit

--- seminar_2/bar_chart/canvas-bar-chart-library.js
+++ seminar_2/bar_chart/canvas-bar-chart-library.js
@@ -21,7 +21,7 @@ class BarChart{
     draw(values){
            const context = this.#canvas.getContext('2d');
            //metodele cu fill deseneaza forme pline, metodele stroke deseneaza doar conturul
-           context.fillStyle = "#DEDEDE"  // rosu,s..., verde
+           context.fillStyle = "#DEDEDE"  // rosu,verde,albastru
            context.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
 
            // ... operatorul asta face conversia de la vector in valori separate, Spread syntax 
@@ -34,10 +34,11 @@ class BarChart{
            for(let i = 0; i< values.length; i++){
                const barX = i * barWidth;
                const barHeight = values[i] * f;
-               const barY = 0;
+               const barY = this.#canvas.height - barHeight;
 
 
-               context.fillRect(barX, barY, barWidth, barHeight)
+               context.fillRect(barX, barY, barWidth/2, barHeight)
+               context.strokeRect(barX,barY,barWidth/2,barHeight)
            }
     }
 }
